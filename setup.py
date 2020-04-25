5d369745911611cafbe1528cadf57ee2ba0854cf
Oprel <foprel@deloitte.nl>
2020-04-25 18:36:40 +0200

    Created PyPi distribution

--- setup.py
+++ setup.py
@@ -7,13 +7,21 @@ with open('README.rst') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+requirements = [
+    "et-xmlfile==1.0.1",
+    "jdcal==1.4.1",
+    "openpyxl==3.0.3",
+    ]
+
 setup(
     name='openpyxl-dictreader',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple package to read openpyxl worksheets like a csv DictReader',
     long_description=readme,
     author='Frank Oprel',
+    author_email="oprel.fj@gmail.com",
     url='https://github.com/foprel/openpyxl-dictreader',
+    install_requires=requirements,
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
