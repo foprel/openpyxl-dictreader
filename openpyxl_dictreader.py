51a3e0022e7e2b53936531b4c41a2408cdff4a41
arkhan <arkhan@riseup.net>
2021-07-27 12:54:24 -0500

    [FIX] default sheet

--- openpyxl_dictreader.py
+++ openpyxl_dictreader.py
@@ -8,10 +8,18 @@ def reader(iterator):
 
 
 class DictReader:
-
-    def __init__(self, filename, worksheet, fieldnames=None, restval=None, restkey=None, *args, **kwargs):
+    def __init__(
+        self,
+        filename,
+        worksheet="",
+        fieldnames=None,
+        restval=None,
+        restkey=None,
+        *args,
+        **kwargs
+    ):
         self.wb = load_workbook(filename, **kwargs)
-        self.ws = self.wb[worksheet]
+        self.ws = worksheet and self.wb[worksheet] or self.wb.active
         self.reader = reader(self.ws)
         self._fieldnames = fieldnames
         self.restkey = restkey
