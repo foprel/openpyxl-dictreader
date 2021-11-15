3d1abdf2428e58dce09d571740dafc67aaabbe0a
Kevin Brown <github@whidit.com>
2021-11-15 16:44:47 -0700

    del __init__.py from src folder
    Oops. It belongs in the `tests` folder

--- tests/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import openpyxl_dictreader
-
-reader = openpyxl_dictreader.DictReader("names.xlsx", "Sheet1")
-for row in reader:
-    print(row["First Name"], row["Last Name"])
