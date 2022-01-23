deb66b0b9917f2f41b759a75df6d753300f40d56
frank <oprel.fj@gmail.com>
2022-01-23 16:31:04 +0100

    Updated to version 0.1.6

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 with open('README.md') as f:
     readme = f.read()
 
@@ -8,14 +7,12 @@ with open('LICENSE') as f:
     license = f.read()
 
 requirements = [
-    "et-xmlfile==1.0.1",
-    "jdcal==1.4.1",
-    "openpyxl==3.0.3",
-    ]
+    "openpyxl~=3.0.3"
+]
 
 setup(
     name='openpyxl-dictreader',
-    version='0.1.4',
+    version='0.1.6',
     description='A simple package to read openpyxl worksheets like a csv DictReader',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -26,4 +23,3 @@ setup(
     license=license,
     py_modules=["openpyxl_dictreader"]
 )
-
