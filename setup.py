b453ab43f886e8a8346e4f78c6695cdd19a78de2
arkhan <arkhan@riseup.net>
2022-05-16 17:11:25 -0500

    [FIX] python dependencies

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.md') as f:
+
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 requirements = [
-    "openpyxl~=3.0.3"
+    "openpyxl==2.6.4 ; python_version < '3.0' ",
+    "openpyxl~=3.0.3 ; python_version >= '3.0' ",
 ]
 
+
 setup(
-    name='openpyxl-dictreader',
-    version='0.1.8',
-    description='A simple package to read openpyxl worksheets like a csv DictReader',
+    name="openpyxl-dictreader",
+    version="0.1.8",
+    description="A simple package to read openpyxl worksheets like a csv DictReader",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Frank Oprel',
+    long_description_content_type="text/markdown",
+    author="Frank Oprel",
     author_email="oprel.fj@gmail.com",
-    url='https://github.com/foprel/openpyxl-dictreader',
+    url="https://github.com/foprel/openpyxl-dictreader",
     install_requires=requirements,
     license=license,
-    py_modules=["openpyxl_dictreader"]
+    py_modules=["openpyxl_dictreader"],
 )
